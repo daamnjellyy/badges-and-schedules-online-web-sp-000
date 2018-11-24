345771d6a9b7c09721e7edee598d3df1202fec91
DANIELLE PARK <dpark1008@gmail.com>
2018-11-24 06:21:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 # Write your code here.
 def badge_maker(name)
-  returns "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
 #batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
