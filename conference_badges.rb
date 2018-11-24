83a5dba6a04177e3e882c1a9207552eaf6d40c15
DANIELLE PARK <dpark1008@gmail.com>
2018-11-24 06:19:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,5 @@
 # Write your code here.
+def batch_badge_creator(names)
 batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 batch_badge_creator.each do |names|
   puts "Hello, my name is #{name}."
